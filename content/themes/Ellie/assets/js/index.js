acd6bf9808caed11ac171751c6a9c27c315d9591
招牌疯子 <zp@buaa.us>
2014-06-14 17:19:33 +0800

    add vim keybind

--- content/themes/Ellie/assets/js/index.js
+++ content/themes/Ellie/assets/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
 	$('pre').addClass('prettyprint');
-	
+
 	mainImage = $('img[alt="main-image"]');
-	
+
 	if ( mainImage.length > 0){
 		mainImageSource = mainImage.attr('src');
 		$('header').css('background-image','url('+mainImageSource+')');
@@ -11,3 +11,24 @@ $(document).ready(function(){
 	}
 
 });
+
+/*模拟键盘上下滚动*/
+window.onload = function(){
+	document.onkeydown = function(e) {
+	    e = e || window.event;
+	    if (e.keyCode == '74') {
+	        $('html,body').stop();
+	        $('html,body').animate({
+	            scrollTop: $(window).stop().scrollTop() + 200
+	        },'fast')
+	    }
+	    else if (e.keyCode == '75') {
+	        $('html,body').stop();
+	        $('html,body').animate({
+	            scrollTop: $(window).stop().scrollTop() - 200
+	        },'fast')
+	    }
+
+	}
+};
+
